2dda7510fa28a507a7e70a38d11f416435d1e768
Bouillon21 <b.barzhanov@yandex.ru>
2025-01-20 17:30:47 +0300

    lab 1 ver 2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(1, -2, 3, 4, 5, -6);
-
+        if (args.length != 1) {
+            System.out.println("Аргуенты должны идти через запятую");
+            return;
+        }
+        List<Integer> numbers  = Arrays.stream(args[0].split(",")).map(Integer::parseInt).toList();
         int evenPositiveCount = 0;
         int oddPositiveCount = 0;
 
